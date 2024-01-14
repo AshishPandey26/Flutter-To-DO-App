1bbaf57d8b443498b1b2024c7c536d181dbd9326
Ashish Pandey <pandaya10@gmail.com>
2024-01-14 22:51:32 +0530

    Create todo_tile.dart

---
+++ lib/utils/todo_tile.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ToDoTile extends StatelessWidget {
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+
+  ToDoTile({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
+      child: Container(
+        padding: EdgeInsets.all(24),
+        child: Row(
+          children: [
+            // checkbox
+            Checkbox(value: taskCompleted,
+                onChanged: onChanged,
+            activeColor: Colors.black,),
+            //tasks name
+            Text(taskName),
+          ],
+        ),
+        decoration: BoxDecoration(
+            color: Colors.yellow, borderRadius: BorderRadius.circular(12)),
+      ),
+    );
+  }
+}
