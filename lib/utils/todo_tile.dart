ca1c3fcc7e486cccbd477e6adfa143d7cd8350e4
Ashish Pandey <pandaya10@gmail.com>
2024-01-15 23:17:34 +0530

    Update todo_tile.dart

--- lib/utils/todo_tile.dart
+++ lib/utils/todo_tile.dart
@@ -1,38 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ToDoTile extends StatelessWidget {
   final String taskName;
   final bool taskCompleted;
   Function(bool?)? onChanged;
+  Function(BuildContext)? deleteFunction;
 
   ToDoTile({
     super.key,
     required this.taskName,
     required this.taskCompleted,
     required this.onChanged,
+    required this.deleteFunction,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
-      child: Container(
-        padding: EdgeInsets.all(24),
-        child: Row(
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
           children: [
-            // checkbox
-            Checkbox(value: taskCompleted,
-                onChanged: onChanged,
-            activeColor: Colors.black,),
-            //tasks name
-
-            Text(taskName,
-            style: TextStyle(decoration:
-            taskCompleted? TextDecoration.lineThrough : TextDecoration.none),)
+            SlidableAction(onPressed: deleteFunction,
+            icon: Icons.delete,
+            backgroundColor: Colors.redAccent,
+            borderRadius: BorderRadius.circular(10),)
           ],
         ),
-        decoration: BoxDecoration(
-            color: Colors.yellow, borderRadius: BorderRadius.circular(12)),
+        child: Container(
+          padding: EdgeInsets.all(24),
+          child: Row(
+            children: [
+              // checkbox
+              Checkbox(value: taskCompleted,
+                  onChanged: onChanged,
+              activeColor: Colors.black,),
+              //tasks name
+
+              Text(taskName,
+              style: TextStyle(decoration:
+              taskCompleted? TextDecoration.lineThrough : TextDecoration.none),)
+            ],
+          ),
+          decoration: BoxDecoration(
+              color: Colors.yellow, borderRadius: BorderRadius.circular(12)),
+        ),
       ),
     );
   }
