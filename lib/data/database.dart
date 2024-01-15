4e2941fedb11312f5a11c4f4c550b53be9556b89
Ashish Pandey <pandaya10@gmail.com>
2024-01-15 23:33:18 +0530

    Update database.dart

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -1,3 +1,5 @@
+import 'package:hive/hive.dart';
+// import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 
 class ToDoDatabase{
@@ -5,6 +7,22 @@ class ToDoDatabase{
   List toDoList = [];
 
   //reference the box
-  final _myBox = Hive.openBox('mybox');
+  final _myBox = Hive.box('mybox');
 
+  // run this methodd if first time opening the app
+  void createInitialData(){
+    toDoList=[
+      ["make Tutorila", false],
+      ["Do Exercise", false],
+    ];
+  }
+  //load data from the database
+  void loadData(){
+    toDoList = _myBox.get("TODOLIST");
+  }
+
+  //update the database
+  void updateDatabase(){
+    _myBox.put("TODOLIST", toDoList);
+  }
 }
